8cc42d8b3e1d523da256b97198c3ccf17b0ca63b
skyDe <sky_de1991@outlook.com>
2024-07-19 09:15:34 -0800

    appolo server initialized / needs run and connect to db / dummyData added for test

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,30 @@
-import { connectDB } from "./connectDB";
-import express, { Request, Response } from "express";
+// import { connectDB } from "./connectDB";
+// import express, { Request, Response } from "express";
+// import env from "dotenv";
+// import cors from "cors";
 
-const env = require("dotenv");
-const cors = require("cors");
-// const express = require("express");
+// env.config();
+// const app = express();
 
-env.config();
-const app = express();
+// app.use(cors());
+// app.get("/", (req: Request, res: Response) =>
+//   res.send("Welcome to my TypeScript express server")
+// );
 
-app.use(cors());
-app.get("/", (req: Request, res: Response) =>
-  res.send("Welcome to my TypeScript express server")
-);
+// const PORT = process.env.PORT || 2323;
+// connectDB()
+//   .then(() =>
+//     app.listen(PORT, () => console.log(`server is running on PORT: ${PORT}`))
+//   )
+//   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 2323;
-connectDB()
-  .then(() =>
-    app.listen(PORT, () => console.log(`server is running on PORT: ${PORT}`))
-  )
-  .catch((err) => console.log(err));
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server);
+console.log(`Server ready at ${url}`);
