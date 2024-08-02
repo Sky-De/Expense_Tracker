6bccb3dec85a52f10ba8374d7bc48542bc416ecf
skyDe <sky_de1991@outlook.com>
2024-08-02 11:10:55 -0800

    userResolver updated / user added / mutation added

--- server/resolvers/userResolver.js
+++ server/resolvers/userResolver.js
@@ -3,7 +3,11 @@ import { users } from "../dummyData/data.js";
 const userResolver = {
   Query: {
     users: () => users,
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
+    },
   },
+  Mutation: {},
 };
 
 export default userResolver;
